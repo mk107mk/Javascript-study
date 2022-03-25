005869ff4e81d69f79a3239f891a1e7289d5e3f1
unknown <chzhclq17@naver.com>
2022-03-25 22:42:06 +0900

    assignment update

--- 문제/1.js
+++ 문제/1.js
@@ -7,9 +7,9 @@
 // function sum()
 
 function sum(num1,num2,num3){
-    num1 = num1??0;
-    num2 = num2??0;
-    num3 = num3??0;
+    num1 ??= 0;
+    num2 ??= 0;
+    num3 ??= 0;
 
     return num1 + num2 + num3;
 }
